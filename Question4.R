2c61dc9b9ba01fb2f69ed90eb80d5f1939da527b
nmubarik <128054394+nmubarik@users.noreply.github.com>
2023-03-18 20:34:17 +0000

    Add files via upload

--- Question4.R
+++ Question4.R
@@ -2,13 +2,15 @@
 #### Question 4
 #######
 
+
 #### Load data
 source("DataCleaning.R")
 source("Question1.R")
 
 ####
 library(factoextra)
-
+library(cluster)
+library(NbClust)
 
 ### What variables would be important to group the houses into clusters
 ### We will use K-means clustering
@@ -30,11 +32,12 @@ house_data_numeric_noNA <- subset(house_data_numeric_noNA,
 
 house_data_numeric_noNA_scaled <- as.data.frame(scale(as.matrix(house_data_numeric_noNA)))
 
-my.km <- kmeans(as.matrix(house_data_numeric_noNA_scaled), centers = 5, nstart = 20)
+my.km <- kmeans(as.matrix(house_data_numeric_noNA_scaled), centers = 3, nstart = 20)
 
 table(my.km$cluster)
 
 house_data_numeric_noNA$cluster <- as.factor(my.km$cluster)
+
 
 ###
 ### 3. Plot
@@ -81,3 +84,25 @@ p8 <- fviz_cluster(my.km, data = subset(house_data_numeric_noNA, select = -clust
 cowplot::plot_grid(p5, p6, p7, p8, labels = c("A. Clusters by Sale Price and Living Area SF", "B. Clusters by Neighborhood and Year Built", "C. Clusters by Garage Type and Overall Condition", "D. Clusters by First Two Principal Components"),
                           hjust = -0.1, vjust = 1.3, label_size = 12)
 
+
+options(scipen=999)
+p9 <- fviz_nbclust(house_data_numeric_noNA, kmeans, method = "wss")
+p9
+
+p10 <- fviz_nbclust(house_data_numeric_noNA,
+                    kmeans,
+                    method = "silhouette")
+p10
+
+gap_stat <- clusGap(subset(house_data_numeric_noNA, select = -cluster), FUNcluster = kmeans, nstart = 25,
+                    K.max = 10, B = 50)
+print(gap_stat, method = "firstmax")
+p11 <- fviz_gap_stat(gap_stat)
+p11
+
+top <- cowplot::plot_grid(p10, p11, labels = c("A. Average Silhoutte Method", "       B. Gap Statistic Method"),
+                          hjust = -1.2)
+bottom <- cowplot::plot_grid(p9, labels = c("C. Elbow Method"),
+                             hjust = -6.0,
+                             vjust = 2.5)
+cowplot::plot_grid(top, bottom, ncol = 1)
